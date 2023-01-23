f8c485299fed0bd6c680a09e6131cec863ed8e89
Lee McDonald <leemcdon@amazon.com>
2023-01-23 14:54:33 +0800

    Initial push of code repo

---
+++ data-gen/data_genrator.py
@@ -0,0 +1,193 @@
+import os
+import sys
+import json
+import time
+import boto3
+import random
+import numpy as np
+import argparse
+from faker import *
+import faker_commerce
+
+
+# Create a client with aws service and region
+def create_client(service, region):
+    return boto3.client(service, region_name=region)
+
+
+def generate_products(fake, number_of_products=100):
+    product_catalogue = []
+    for product_number in range(1, number_of_products):
+        product = {
+            "sku": "product-" + str(fake.uuid4()),
+            "name": fake.ecommerce_name(),
+            "type": fake.ecommerce_category(),
+            "price": fake.ecommerce_price()
+        }
+
+        product_catalogue.append(product)
+
+    return product_catalogue
+
+
+def generate_fake_purchase(fake, sold_products, purchase_timestamp):
+
+    # Scale to create multiple records
+    purchase = {
+        "body": {
+            "purchase_time": purchase_timestamp,
+            "transaction_id": "T-" + str(fake.uuid4()),
+            "user_id": "U-" + str(fake.uuid4()),
+            "basket_items": []
+        }
+    }
+    for product in sold_products:
+        basket_item = {
+            "product": {
+                "sku": product["sku"],
+                "name": product["name"],
+                "category": product["type"],
+                "pricing": {
+                    "price": product["price"],
+                },
+                "quantity_purchased": random.randint(0, 3)
+            }
+        }
+        purchase["body"]["basket_items"].append(basket_item)
+
+    return purchase
+
+
+def generate_prediction(purchase, percent_late, lateness, product_catalog):
+
+    next_best_product = random.choice(product_catalog)
+    late_timestamp = get_time_stamp_and_simulate_lateness_for_records(percent_late, time_delay=lateness)
+    prediction = {
+        "purchase_id": purchase['body'].get('transaction_id'),
+        "purchase_time": purchase['body'].get('purchase_time'),
+        "likelihood_score": int(np.random.choice(np.arange(1, 11), p=[0.3, 0.2, 0.1, 0.1, 0.1, 0.1, 0.04, 0.03, 0.02, 0.01])),
+        "prediction_timestamp": late_timestamp,
+        "next_best_product_sku": next_best_product["sku"]
+    }
+
+    return prediction
+
+
+def get_time_stamp_and_simulate_lateness_for_records(percent_late, time_delay, default_time_delay=1):
+
+    if percent_late > 0:
+        value = random.random() * 100
+        if (value >= percent_late):
+            return int(time.time())
+        else:
+            return (int(time.time()) + time_delay)
+    else:
+        # Default delay for the down stream processing is 1 second
+        return int(time.time() + default_time_delay)
+
+    return time.time()
+
+
+def get_purchase_records(fake, product_catalog, percent_late=20, lateness=5):
+    purchases = []
+    predictions = []
+    rate_of_records_produced_per_interval = 1
+    for r in range(0, rate_of_records_produced_per_interval):
+
+        purchase_timestamp = int(time.time())
+
+        sold_products = random.choices(product_catalog, k=random.randint(1, 5))
+        purchase = generate_fake_purchase(fake, sold_products, purchase_timestamp)
+        purchase_data = json.dumps(purchase)
+
+        purchase_json_payload = {
+            'Data': bytes(purchase_data, 'utf-8'),
+            'PartitionKey': 'partition_key'
+        }
+        purchases.append(purchase_json_payload)
+
+        prediction_result = generate_prediction(purchase, percent_late, lateness, product_catalog)
+        prediction_result_json_payload = {
+            'Data': bytes(json.dumps(prediction_result), 'utf-8'),
+            'PartitionKey': 'partition_key'
+        }
+        predictions.append(prediction_result_json_payload)
+
+    return purchases, predictions
+
+
+def send_records_to_kinesis(kinesis_client, fake, product_catalog, job_parameters, fraud_service_delay=2, percent_late=33, lateness=5):
+
+    while True:
+
+        fake_purchases, fake_recommendations = get_purchase_records(
+            fake, product_catalog, percent_late=percent_late, lateness=lateness)
+
+        print(f"\n\n----fake purchases [{len(fake_purchases)}]----")
+        print(fake_purchases)
+
+        product_write_response = kinesis_client.put_records(
+            StreamName=job_parameters.purchase_stream,
+            Records=fake_purchases
+        )
+        print(f"----fake product_write_response [{product_write_response}]----")
+
+        if fraud_service_delay > 0:
+            time.sleep(float(fraud_service_delay))
+
+        print(f"\n\n----fake recommendation [{len(fake_recommendations)}]----")
+        print(fake_recommendations)
+
+        recommendation_write_response = kinesis_client.put_records(
+            StreamName=job_parameters.recommender_stream,
+            Records=fake_recommendations
+        )
+        print(f"----fake recommendation_write_response [{recommendation_write_response}]----")
+
+
+def main(args):
+
+    # print (args)
+    # Make sure to set your profile here
+    session = boto3.Session(
+        aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
+        aws_session_token=os.environ["AWS_SESSION_TOKEN"],
+        aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
+    )
+
+    try:
+        # Initialize Faker library
+        faker = Faker()
+        faker.add_provider(faker_commerce.Provider)
+
+        # Kinesis settings
+        kinesis_client = session.client('kinesis', args.region)
+
+        # Generate 1000 different Users
+        product_catalog = generate_products(faker, number_of_products=1000)
+
+        # Create fake stream of data and send it to kinesis streams
+        send_records_to_kinesis(kinesis_client, faker, product_catalog, args)
+
+    except:
+        print("Error:", sys.exc_info()[0])
+        raise
+
+
+if __name__ == "__main__":
+    # run main
+    parser = argparse.ArgumentParser(description='Faker based streaming data generator')
+
+    parser.add_argument('--recommenderstreamname', action='store', dest='recommender_stream',
+                        default='recommenderStream',
+                        help='Provide Kinesis Data Stream name to recommender stream data')
+
+    parser.add_argument('--purchasestreamname', action='store', dest='purchase_stream',
+                        default='nestedPurchaseStream',
+                        help='Provide Kinesis Data Stream name to purchase stream data')
+
+    parser.add_argument('--region', action='store', dest='region', default='ap-southeast-2')
+
+    args = parser.parse_args()
+
+    main(args)
---
+++ src/streaming_join_job.py
@@ -0,0 +1,114 @@
+import sys
+from awsglue.transforms import *
+from awsglue.utils import getResolvedOptions
+from awsglue.context import GlueContext
+from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
+from pyspark.context import SparkContext
+from pyspark.sql.functions import explode, col, expr, from_unixtime, current_timestamp
+from pyspark.sql import DataFrame
+import datetime
+
+
+args = getResolvedOptions(sys.argv,
+                          ['JOB_NAME',
+                           'output_path'])
+
+job_name = args['JOB_NAME']
+output_path = args['output_path']
+checkpoint_location = output_path + "/" + job_name + "/checkpointing"
+s3_target = output_path  + "/" + job_name  +  "/dataOutput"
+
+sc = SparkContext.getOrCreate()
+
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(job_name, args)
+
+logger = glueContext.get_logger()
+
+
+def process_batch(joined_df, batch_id):
+
+    logger.info(f" ========== number of joined records {joined_df.count()} ==============")
+    if joined_df.count() > 0:
+        joined_gdf = DynamicFrame.fromDF(joined_df, glueContext, "from_data_frame")
+
+        joined_gdf.printSchema()
+        joined_df.show(vertical=True, truncate=False)
+
+        now = datetime.datetime.now()
+        s3path = s3_target + "/ingest_year=" + "{:0>4}".format(str(now.year)) \
+                 + "/ingest_month=" + "{:0>2}".format(str(now.month)) \
+                 + "/ingest_day=" + "{:0>2}".format(str(now.day)) \
+                 + "/ingest_hour=" + "{:0>2}".format(str(now.hour)) \
+                 + "/ingest_minute=" + "{:0>2}".format(str(now.minute)) + "/"
+
+        s3sink = glueContext.write_dynamic_frame.from_options(
+            frame=joined_gdf,
+            connection_type="s3",
+            connection_options={"path": s3path},
+            format = "parquet",
+            transformation_ctx="s3sink"
+        )
+        logger.info(f" ========== Batch [{batch_id}] Write Results {s3sink} ==============")
+
+
+def flatten_nested_purchase(nested_json_df: DataFrame) -> DataFrame:
+
+    raw_output_df = nested_json_df \
+        .withColumn("basket_items", explode("body.basket_items")) \
+        .select("body.purchase_time", "body.transaction_id", "body.user_id", "basket_items.*")
+
+    output_df = raw_output_df.select("*", "product.*").drop("product").select("*", "pricing.*").drop("pricing")
+
+    return output_df
+
+
+nested_product_kinesis_df = glueContext.create_data_frame_from_catalog(
+    database="awsglue-streaming-join-nested-json-db",
+    table_name="nested-purchase-stream-table",
+    transformation_ctx="nested_purchase_ds",
+    additional_options={"startingPosition": "latest"}
+)
+
+flattened_products_df = flatten_nested_purchase(nested_product_kinesis_df)
+flattened_products_ts_df = flattened_products_df.withColumn("purchase_time", from_unixtime(col("purchase_time")).cast("timestamp"))
+flattened_products_with_watermark_df = flattened_products_ts_df.withWatermark("purchase_time", "10 minutes")
+
+recommendations_kinesis_df = glueContext.create_data_frame_from_catalog(
+   database="awsglue-streaming-join-nested-json-db",
+   table_name="recommender-stream-table",
+   transformation_ctx="recommender_ds",
+   additional_options={"startingPosition": "latest"}
+)
+
+recommendations_with_watermark_df = recommendations_kinesis_df\
+    .withColumn("original_purchase_time", from_unixtime(col("purchase_time")).cast('timestamp'))\
+    .withWatermark("original_purchase_time", "30 seconds")\
+    .withColumn("recommendation_processed_time", current_timestamp())\
+    .drop(col("purchase_time"))
+
+
+flattened_products_with_recommendation_df = flattened_products_with_watermark_df.join(
+  recommendations_with_watermark_df,
+  expr("""
+        transaction_id = purchase_id AND
+        recommendation_processed_time >= purchase_time AND
+        recommendation_processed_time >= purchase_time + interval 30 SECONDS
+    """), "inner"
+)
+
+
+glueContext.forEachBatch(
+    frame=flattened_products_with_recommendation_df,
+    batch_function=process_batch,
+    options={
+        "windowSize": "10 seconds",
+        "checkpointLocation": checkpoint_location
+    }
+)
+
+job.commit()
+
